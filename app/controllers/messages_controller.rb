7ec6899e9684cb12d60d2ed8d6fff16608c30a20
ribrciti <rbciti@msn.com>
2015-07-07 09:47:50 -0700

    Added Simple form, bootstrap, new messages and related views

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,45 @@
+class MessagesController < ApplicationController
+	before_action :find_message, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@messages = Message.all.order("created_at DESC")
+	end
+
+	def show
+	end
+
+	def new
+		@message = Message.new
+	end
+
+	def create
+		@message = Message.new(message_params)
+		if @message.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+	private
+
+	def message_params
+		params.require(:message).permit(:title, :description)
+	end
+
+	def find_message
+		@message = Message.find(params[:id])
+	end
+end
