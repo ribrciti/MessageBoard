fb9e795440e90a29896ce203a89f6a6c01f10cb5
ribrciti <rbciti@msn.com>
2015-07-10 09:59:23 -0700

    Added nested comments, views, controller actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708125215) do
+ActiveRecord::Schema.define(version: 20150708150443) do
+
+  create_table "comments", force: true do |t|
+    t.text     "content"
+    t.integer  "message_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["message_id"], name: "index_comments_on_message_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "messages", force: true do |t|
     t.string   "title"
